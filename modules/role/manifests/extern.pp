bee753a8908b8c1aa7ca01e6b5940ffbd759a8eb
Mark Y. Goh <mgoh@cca.edu>
2014-10-30 16:52:00 -0700

    removed

--- modules/role/manifests/extern.pp
+++
@@ -1,4 +0,0 @@
-class role::extern {
-  include fail2ban
-  monit::service { 'fail2ban': }
-}
--- modules/role/manifests/params.pp
+++
@@ -1,9 +0,0 @@
-class role::params {
-  case $::lsbdistcodename {
-    'lenny', 'squeeze', 'maverick', 'natty': {
-    }
-    default: {
-      fail("Module ${module_name} does not support ${::lsbdistcodename}")
-    }
-  }
-}
