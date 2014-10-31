6d83bdcf1d87844bbbd80037ec71c7413272fecb
Mark Y. Goh <mgoh@cca.edu>
2014-10-31 16:24:16 -0700

    added basic vhost

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,12 @@
 node 'vm-webhost-01.cca.edu' {
+  #ntp module for correc time
   include '::ntp'
     class { '::ntp':
     servers => [ 'ntp1.cca.edu', 'ntp2.cca.edu' ],
+  }
+  #apache setup
+  apache::vhost { 'wwwtest1.cca.edu':
+    port    => '80',
+    docroot => '/opt/www/first',
   }
 }
