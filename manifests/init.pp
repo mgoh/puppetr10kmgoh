afa889ad20b18973db1243108565dfe7cfb2756c
Mark Y. Goh <mgoh@cca.edu>
2014-10-31 16:42:34 -0700

    rm apache

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,7 @@
 node 'vm-webhost-01.cca.edu' {
-  #ntp module for correc time
+  #ntp module for correct time
   include '::ntp'
     class { '::ntp':
     servers => [ 'ntp1.cca.edu', 'ntp2.cca.edu' ],
-  }
-  #apache setup
-  apache::vhost { 'wwwtest1.cca.edu':
-    port    => '80',
-    docroot => '/opt/www/first',
   }
 }
