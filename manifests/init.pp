ac024c1d1368d7772fcfae9950154111ef32d0c2
Mark Y. Goh <mgoh@cca.edu>
2014-11-11 14:45:49 -0800

    removed vm-webhost-02 for demo

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,24 +15,6 @@ node 'vm-webhost-01.cca.edu' {
     path         => "${::apache::params::lib_path}/libphp5.so",
   }
 }
-node 'vm-webhost-02.cca.edu' {
-  #ntp module for correct time
-  class { '::ntp':
-    servers => [ 'ntp1.cca.edu', 'ntp2.cca.edu' ],
-  }
-  class { '::apache':
-    mpm_module => 'prefork'
-  }
-  apache::vhost { 'precious.cca.edu':
-    port       => '80',
-    docroot    => '/opt/www/precious',
-  }
-  class {'::apache::mod::php':
-    package_name => "php5",
-    path         => "${::apache::params::lib_path}/libphp5.so",
-  }
-}
-
 node 'vm-mysql-07.cca.edu' {
   #ntp module for correct time
   class { '::ntp':
